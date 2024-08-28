731b5b2aea703cf72d1b5f073afcf6a71a3c4232
ganesh baidi <ganeshbaidi18@gmail.com>
2024-08-28 13:21:53 +0530

    firstcommit

---
+++ src/Components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import linkedin from './linkedin.png'
+export default function Footer() {
+  return (
+    <div class="d-flex align-items-center justify-content-between" style={{position:'fixed',bottom:'0vw',width:'100vw',height:'7vw',backgroundColor:'black',color:'white',zIndex:99999}} >
+        <div class="container text-center">
+            
+   Developed by Ganesh Baidi 
+     <a href="https://www.linkedin.com/in/ganesh-baidi-ba00b9241/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"><img src={linkedin} alt="linkedin" style={{height:'3vw',width:'3vw',marginLeft:'1.5vw',marginTop:'-0.4vw'}}></img></a>
+    
+     </div>
+        </div>
+  )
+}
